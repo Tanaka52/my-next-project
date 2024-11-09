39c625673ce39b94801f5fe5ee945f320062edd4
Tanaka52 <ktc24a31e0008@edu.kyoto-tech.ac.jp>
2024-11-09 16:32:09 +0900

    komi

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,9 @@
 import styles from "./page.module.css";
 import Image from "next/image";
 
-
+import NewsList from "@/app/_components/NewsList";
 import ButtonLink from "@/app/_components/ButtonLink";
-
-type News = {
-  id: string;
-  title: string;
-  category: {
-    name : string;
-  };
-  publishedAt: string;
-  createdAt: string
-  };
+import { News } from "@/app/_libs/microcms";
 
 const data: {
   contents: News[] } = {
@@ -49,62 +40,31 @@ const data: {
 };
   
 export default function Home() {
-  const sliceData = data.contents.slice(0,2);
-
+  const sliceData = data.contents.slice(0, 2);
+  
   return (
     <>
-    <section className={styles.top}>
-    <div>
-  <h1 className={styles.title}>テクノロジーの力で世界を変える</h1>
-  <p className={styles.description}>
-    私たちは市場をリードしているグローバルカンパニーです。</p>
-  </div>
+      <section className={styles.top}>
+      <div>
+        <h1 className={styles.title}>テクノロジーの力で世界を変える</h1>
+        <p className={styles.description}>
+        私たちは市場をリードしているグローバルカンパニーです。</p>
+      </div>
 
-  <Image
-    className={styles.bgimg}
-    src="/img-mv.jpg"
-    alt=""
-    width={4000}
-    height={1200}
-  />
-  </section>
-  <section className={styles.news}>
-    <h2 className={styles.newsTitle}>News</h2>
-      <ul>
-        {sliceData.map((article)=> (
-          <li key = {article.id} className={styles.list}>
-            <div className={styles.link}>
-              <Image
-                className={styles.image}
-                src="/no-image.png"
-                alt="No Image"
-                width={1200}
-                height={630}
-              />
-              <dl className={styles.content}>
-                <dt className={styles.newsItemTitle}>
-                  {article.title}</dt>
-                  <dd className={styles.meta}>
-                    <span className={styles.date}>
-                      <Image
-                      src="/clock.svg"
-                      alt=""
-                      width={16}
-                      height={16}
-                      priority
-                      />
-                      {article.publishedAt}
-                    </span>
-                  </dd>
-                </dl>
-              </div>
-            </li>
-          ))}
-        </ul>
+      <Image
+        className={styles.bgimg}
+        src="/img-mv.jpg"
+        alt=""
+        width={4000}
+        height={1200}
+      />
+      </section>
+      <section className={styles.news}>
+        <h2 className={styles.newsTitle}>News</h2>
+        <NewsList news={sliceData} />
         <div className={styles.newsLink}>
           <ButtonLink href="/news">もっとみる</ButtonLink>
-          </div> 
-          
+        </div> 
       </section>
     </>
   );
