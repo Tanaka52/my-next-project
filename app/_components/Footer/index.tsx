7ff8917197cf62683c2d49756d5ad77de50fde82
Tanaka52 <ktc24a31e0008@edu.kyoto-tech.ac.jp>
2024-11-30 16:18:13 +0900

    コミット

--- app/_components/Footer/index.tsx
+++ app/_components/Footer/index.tsx
@@ -1,3 +1,4 @@
+import Link from'next/link';
 import styles from "./index.module.css";
 
 export default function Footer() {
@@ -6,11 +7,12 @@ export default function Footer() {
             <nav className={styles.nav}>
                 <ul className={styles.items}>
                     <li className={styles.item}>
-                        <a href="/news">ニュース</a>
+                        <Link href="/news">ニュース</Link>
                     </li>
                     <li className={styles.item}></li>
+                        <Link href="/members">メンバー</Link>
                     <li className={styles.item}>
-                        <a href="/contact">お問い合わせ</a>
+                        <Link href="/contact">お問い合わせ</Link>
                     </li>
                 </ul>
             </nav>
